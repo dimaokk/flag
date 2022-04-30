2571e74cd252ebf30d03306aef8a8bf2143348ed
dimaokk <dimadark216@gmail.com>
2022-04-30 17:26:59 +0700

    start-project

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+import { Container } from "../Styled-Components/Container";
+import { GiMoonBats, GiFalconMoon } from "react-icons";
+
+export const Header = () => {
+  return <Container>
+      
+  </Container>;
+};
